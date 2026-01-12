a1cc1e88d7d25d3d2dda3504a9ef71037a8e9f6b
BKarthik7 <bangikarthik7@gmail.com>
2026-01-12 19:43:17 +0530

    Restart Button

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -2,34 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameOverUI : MonoBehaviour {
+public class GameOverUI : MonoBehaviour
+{
 
 
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+    private Button playAgainButton;
 
 
-    private void Start() {
+    private void Start()
+    {
+        playAgainButton = GetComponentInChildren<Button>();
+
         KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        playAgainButton.onClick.AddListener(PlayAgain);
 
         Hide();
     }
 
-    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e) {
-        if (KitchenGameManager.Instance.IsGameOver()) {
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (KitchenGameManager.Instance.IsGameOver())
+        {
             Show();
 
             recipesDeliveredText.text = DeliveryManager.Instance.GetSuccessfulRecipesAmount().ToString();
-        } else {
+        }
+        else
+        {
             Hide();
         }
     }
 
-    private void Show() {
+    private void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Show()
+    {
         gameObject.SetActive(true);
     }
 
-    private void Hide() {
+    private void Hide()
+    {
         gameObject.SetActive(false);
     }
 
